77aefb10dc32428ce3c392b09e22b33d28139d76
Randy Kahle <randy@databliss.net>
2015-11-02 11:58:47 -0700

    Clean up code formatting.

--- src/main/java/com/google/training/helloworld/HelloWorldEndpoints.java
+++ src/main/java/com/google/training/helloworld/HelloWorldEndpoints.java
@@ -8,25 +8,23 @@ import com.google.api.server.spi.config.Named;
 /**
  * Defines endpoint functions APIs.
  */
-@Api(name = "helloworldendpoints", version = "v1",
-scopes = {Constants.EMAIL_SCOPE },
-        clientIds = {Constants.WEB_CLIENT_ID, Constants.API_EXPLORER_CLIENT_ID },
-        description = "API for hello world endpoints.")
-
+@Api(
+    name = "helloworldendpoints", 
+    version = "v1",
+    scopes = {Constants.EMAIL_SCOPE },
+    clientIds = {Constants.WEB_CLIENT_ID, Constants.API_EXPLORER_CLIENT_ID },
+    description = "API for hello world endpoints."
+    )
 public class HelloWorldEndpoints {
 
-   // Declare this method as a method available externally through Endpoints
-    @ApiMethod(name = "sayHello", path = "sayHello",
-            httpMethod = HttpMethod.GET)
-
+   // Make this method available externally through Endpoints
+    @ApiMethod(name = "sayHello", path = "sayHello", httpMethod = HttpMethod.GET)
     public HelloClass sayHello() {
         return new HelloClass();
     }
 
-    // Declare this method as a method available externally through Endpoints
-    @ApiMethod(name = "sayHelloByName", path = "sayHelloByName",
-            httpMethod = HttpMethod.GET)
-
+    // Make this method available externally through Endpoints
+    @ApiMethod(name = "sayHelloByName", path = "sayHelloByName", httpMethod = HttpMethod.GET)
     public HelloClass sayHelloByName (@Named("name") String name) {
         return new HelloClass(name);
     }
